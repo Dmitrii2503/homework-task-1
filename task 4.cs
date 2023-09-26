dcd514c0f900853542d378aba88ab28317b78d13
Dmitrii2503 <102679550+Dmitrii2503@users.noreply.github.com>
2023-09-26 11:01:37 +0300

    Add files via upload

---
+++ task 4.cs
@@ -0,0 +1,14 @@
+﻿int n = 0 ;
+
+while (n<5)     
+{ 
+    if ( n % 2 == 0 )
+    {
+        Console.Write ("your number is  ");
+        Console.Write (n + ", ");
+    };
+    
+    n += 1; 
+}; 
+
+// так же работает с другими числами 
