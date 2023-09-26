031000e186457218be91dcf6cdcdbbff6e3de340
Dmitrii2503 <dastashenko@inbox.ru>
2023-09-26 09:51:03 +0300

    first task complited

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 5; 
+int b = 7;
+
+int max = b;
+
+if (a>max) 
+{
+    Console.Write ("max = ");
+    Console.Write (a);
+}
+
+else 
+{
+Console.Write ("max = ");
+Console.Write (max);
+}
+
+// возможно подставить другие значение, в таком случае output будет : 
+// max = 10 ; max = -3; 
