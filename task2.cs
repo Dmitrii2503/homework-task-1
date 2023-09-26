356527c58500d89bf0870bed36ae36426395205e
Dmitrii2503 <102679550+Dmitrii2503@users.noreply.github.com>
2023-09-26 10:30:41 +0300

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,20 @@
+﻿int a = 2; 
+int b = 3; 
+int c = 7; 
+
+int max = c; 
+
+
+if (a>max) max = a ;
+
+
+if (b>max) max = b ;
+
+
+if (c>max) max = c ;
+
+
+Console.WriteLine (max);
+
+
+// так же можо поставить другие значения и все будет работать 
